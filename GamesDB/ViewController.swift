d159c1082464f2a26e633f25c52903aeca9ac9ed
esaygiver <saygivere@gmail.com>
2021-03-10 12:03:00 +0300

    Pods Added

---
+++ GamesDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GamesDB
+//
+//  Created by admin on 10.03.2021.
+//  Copyright © 2021 esaygiver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '12.0'
+
+target 'GamesDB' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for GamesDB
+
+pod 'Moya', '~> 14.0'
+pod 'Kingfisher', '~> 6.0'
+
+end
